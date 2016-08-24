4faca93900b473585bc989900b044be8ac9f51bb
Sweta Sawkar <sweta.s@ECHIDNA.COM>
2016-08-24 11:22:57 +0530

    hold file

--- src/app/hold/hold.js
+++ src/app/hold/hold.js
@@ -12,14 +12,32 @@ function HoldConfig( $stateProvider ) {
 			controller: 'HoldCtrl',
 			controllerAs: 'hold',
 			resolve:{
-				Order: function(OrderCloud, $q, $stateParams){
+				Order: function(OrderCloud, $q, $stateParams, LineItemHelpers){
 					var dd=$q.defer();
 					OrderCloud.Orders.ListOutgoing(null, null, $stateParams.orderID, null, null, 'ID').then(function(res){
 						console.log(res);
 						OrderCloud.LineItems.List(res.Items[0].ID, null, null, null, null, null, {"xp.Status":'OnHold'}).then(function(data){
 							console.log(data);
-							dd.resolve(data);
+							LineItemHelpers.GetProductInfo(data.Items).then(function(data1){
+								dd.resolve(data);
+							})
+							//dd.resolve(data);
 						})
+					})
+					return dd.promise;
+				},
+				WiredProduct: function(OrderCloud, $q){
+					var dd=$q.defer();
+					OrderCloud.Products.List(null, null, null, null, null, {"xp.WireService": true}).then(function(res){
+						console.log(res);
+						/*OrderCloud.LineItems.List(res.Items[0].ID, null, null, null, null, null, {"xp.Status":'OnHold'}).then(function(data){
+							console.log(data);
+							LineItemHelpers.GetProductInfo(data.Items).then(function(data1){
+								dd.resolve(data);
+							})
+							//dd.resolve(data);
+						})*/
+						dd.resolve(res);
 					})
 					return dd.promise;
 				}
@@ -28,21 +46,70 @@ function HoldConfig( $stateProvider ) {
 }
 
 
-function HoldController($scope, $stateParams, Order) {
+function HoldController($scope, $stateParams, Order, WiredProduct) {
 	var vm = this;
-	vm.onholdlineitems=Order;
+	vm.onholdlineitems=Order.Items;
+	vm.wiredproducts=WiredProduct;
+	vm.wireserviceopt=null;
 	console.log(vm.onholdlineitems);
+	vm.addItem=function(){
+		if(vm.deliveryinfo==null/* || vm.wireserviceopt==null*/){
+			alert("Please select a lineitem or wireservice Option");
+		}
+		else{
+			vm.selectSKU =! vm.selectSKU;
+		}
+	}
+	$scope.selectFlorist=function(){
+		vm.selectFlorist =! vm.selectFlorist;
+	}
 	$scope.gridOptions = {
-		data: 'hold.onholdlineitems.Items',
+		data: 'hold.onholdlineitems',
 		enableSorting: true,
 		columnDefs: [
-			{ name: '"Allocated"', displayName:'Inventory Status'},
-			{ name: 'DateCreated', displayName:'Order Placed On', cellTemplate: '<div class="data_cell">{{row.entity.DateCreated | date:grid.appScope.dateFormat}}</div>'},
-			{ name: 'FromUserFirstName', displayName:'Sender Name'},
-			{ name: 'BillingAddress', displayName:'Occassions'},
-			{ name: 'Totl', displayName:'Wire Status Code'},
-			{ name: 'xp.CSRID', displayName:'CSR ID'},
-			{ name: 'ShippingCost', displayName:'', cellTemplate: '<div class="data_cell" ui-sref="hold({orderID:row.entity.ID})"><a> <i class="fa fa-upload"></i> Open Order</a></div>', width:"15%"}
+			{ name: 'selectradio ', displayName:' ', cellTemplate: '<div class="data_cell"><input type="radio" name="holdlineitem" ng-click="grid.appScope.showlineitem(row.entity)" /></div>'},
+			{ name: 'ID', displayName:'Inventory Status'},
+			{ name: 'ProductID', displayName:'SKU Code'},
+			{ name: 'Product.Name', displayName:'Product Name'},
+			{ name: 'BillingAddress', displayName:'SKU Option'},
+			{ name: 'Price', displayName:'List Price',cellTemplate: '<div class="data_cell">{{row.entity.UnitPrice|currency}}</div>'},
+			{ name: 'Quantity', displayName:'Qty.'},
+			{ name: 'xp.TotalCost', displayName:'Invoice Price'},
+			{ name: 'ShippingCost', displayName:'Item Discount'},
+			{ name: 'ShippingCost', displayName:'Additional Item Information'}
 		]
 	};
+	$scope.gridskuopt = {
+		data: 'hold.wiredproducts.Items',
+		enableSorting: true,
+		columnDefs: [
+			{ name: 'checkbox', displayName:'', cellTemplate: '<div class="data_cell"><input type="radio" name="dummyprod" ng-click="grid.appScope.dummyprod(row.entity)" /></div>'},
+			{ name: 'ID', displayName:'SKU Code'},
+			{ name: 'Name', displayName:'Product Name'},
+			{ name: 'ProductID', displayName:'List Price'}
+		]
+	};
+
+	$scope.showlineitem=function(lineitem){
+		console.log("lineitem", lineitem);
+		vm.deliveryinfo=lineitem;
+	}
+
+	$scope.dummyprod=function(prod){
+		console.log("prod", prod);
+		vm.dummyproduct=prod;
+	}
+	vm.adddummy = function(){
+		if(vm.dummyproduct!=null){
+			vm.selectSKU =! vm.selectSKU;
+			console.log(vm.dummyproduct);
+			console.log(vm.onholdlineitems);
+			vm.onholdlineitems.push(vm.dummyproduct);
+			console.log("zxczxc",vm.onholdlineitems);
+			$state.reload();
+		}
+		else{
+			alert("enter");
+		}
+	}
 }

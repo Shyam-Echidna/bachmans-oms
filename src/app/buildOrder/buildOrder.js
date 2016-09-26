97a5a091ba099b7103395468fa12637b8512a784
Khasim A <khasim.a@ECHIDNA.COM>
2016-09-26 11:56:59 +0530

    Delivery rule update & PLP loader

--- src/app/buildOrder/buildOrder.js
+++ src/app/buildOrder/buildOrder.js
@@ -1167,6 +1167,10 @@ function buildOrderController($scope, $rootScope, $state, $controller, $statePar
 				vm.AssemblyList.push(key);
 		});
 	};
+	vm.HideUpsellNCross = function(){
+		vm.upsell = true;
+		vm.similar = true;
+	};
 }
 
 function buildOrderTopController($scope, $stateParams,$rootScope, AlfrescoFact) {
@@ -3386,13 +3390,6 @@ function BuildOrderService( $q, $window, $stateParams, ocscope, buyerid, OrderCl
 												line.DeliveryNotAvailable = true;
 										}
 									}
-									dt = angular.copy(CstDateTime).setHours(0, 0, 0, 0);
-									if(angular.copy(CstDateTime).getHours() < 12 && dt == new Date(line.deliveryDate)){
-										obj['Same Day Delivery'] = vm.buyerXp.Shippers.LocalDelivery.StandardDeliveryFees;
-										if((line.xp.addressType == "Funeral" || line.xp.addressType == "Church") && vm.buyerXp.Shippers.LocalDelivery.StandardDeliveryFees > 0){
-											
-										}
-									}
 								}
 								if(line.xp.DeliveryMethod=="UPS"){
 									obj['UPS Charges'] = vm.buyerXp.Shippers.UPS.UPSCharges;
@@ -3410,7 +3407,15 @@ function BuildOrderService( $q, $window, $stateParams, ocscope, buyerid, OrderCl
 								if(line.xp.deliveryFeesDtls){
 									if(line.xp.deliveryFeesDtls['Placement Charges'])
 										obj['Placement Charges'] = line.xp.deliveryFeesDtls['Placement Charges'];
-								}	
+								}
+								dt = angular.copy(CstDateTime).setHours(0, 0, 0, 0);
+								if(angular.copy(CstDateTime).getHours() < 12 && dt == new Date(line.deliveryDate) && (line.xp.DeliveryMethod=="LocalDelivery" || line.xp.DeliveryMethod=="Faster")){
+									obj['Same Day Delivery'] = vm.buyerXp.Shippers.LocalDelivery.StandardDeliveryFees;
+									if((line.xp.addressType == "Funeral" || line.xp.addressType == "Church") && vm.buyerXp.Shippers.LocalDelivery.StandardDeliveryFees > 0){
+										obj = {};
+										obj['Same Day Delivery'] = vm.buyerXp.Shippers.LocalDelivery.StandardDeliveryFees;
+									}
+								}
 								line.xp.deliveryFeesDtls = obj;
 								line.xp.TotalCost = 0;
 								line.xp.deliveryCharges = 0;
